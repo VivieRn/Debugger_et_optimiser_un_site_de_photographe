9e2b6ee311429960ca14da1df00f3f78815b38c4
Nicolas VIVIER <vivier.nicolas0@gmail.com>
2023-05-25 16:41:07 +0200

    Ajout de manifest.json, PWA ready

--- serviceWorker.js
+++ serviceWorker.js
@@ -83,3 +83,11 @@ self.addEventListener("activate", function (event) {
       })
   );
 });
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
