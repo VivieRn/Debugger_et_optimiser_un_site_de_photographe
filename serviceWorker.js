5c5135bc524debd81305765f1bc76e301c8af44f
Nicolas VIVIER <vivier.nicolas0@gmail.com>
2023-06-01 17:05:33 +0200

    Réparation de service worker pour GH page

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,9 +5,10 @@ const registerServiceWorker = async () => {
       if (!registration) {
         // Service worker non enregistré, on l'installe
         const newRegistration = await navigator.serviceWorker.register(
-          "https://viviern.github.io/serviceWorker.js",
+          "https://viviern.github.io/Debugger_et_optimiser_un_site_de_photographe/serviceWorker.js",
           {
-            scope: "https://viviern.github.io/",
+            scope:
+              "https://viviern.github.io/Debugger_et_optimiser_un_site_de_photographe/",
           }
         );
         if (newRegistration.installing) {
