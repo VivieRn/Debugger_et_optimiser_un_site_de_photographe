783934575a729fd067d9ef5af2efe03aa2a745d1
Nicolas VIVIER <vivier.nicolas0@gmail.com>
2023-06-01 16:42:57 +0200

    MAJ white list service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("'https://viviern.github.io/serviceWorker.js", {
-      scope: "'https://viviern.github.io/'",
+    .register("https://viviern.github.io/serviceWorker.js", {
+      scope: "https://viviern.github.io/",
     })
     .then(function (registration) {
       console.log(
@@ -18,6 +18,7 @@ const whitelistedOrigins = [
   "http://localhost",
   "http://localhost:5500",
   "http://127.0.0.1:5500",
+  "https://viviern.github.io/",
 ];
 
 const whitelistedPathRegex = /^\/api\/(?!.*\.(?:jpe?g|png)$)[^/]*$/;
